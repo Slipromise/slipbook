297c0efdea2e5acb9643b33367f8d945339649c3
Slipromise <chengchun05@gmail.com>
2024-03-11 19:30:12 +0800

    TASK-80 新增@storybook/addon-a11y

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -9,6 +9,7 @@ const config: StorybookConfig = {
     "@storybook/addon-interactions",
     "@storybook/addon-styling-webpack",
     "@storybook/addon-themes",
+    "@storybook/addon-a11y",
   ],
   framework: {
     name: "@storybook/nextjs",

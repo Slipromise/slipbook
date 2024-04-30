8ee959c29776b1db9a95f07a5e62e3cf74ed5fa9
Slipromise <chengchun05@gmail.com>
2024-04-30 21:31:36 +0800

    TASK-555 add NeonLightText

---
+++ src/stories/styles/NeonLightText.stories.tsx
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import styles from "./NeonLightText.module.scss";
+import React from "react";
+
+type Props = {
+  className?: React.HtmlHTMLAttributes<HTMLDivElement>["className"];
+};
+
+function NeonLightText({ className }: Props) {
+  return (
+    <div className={className}>
+      <span>N</span>
+      <span>e</span>
+      <span>o</span>
+      <span>n</span>
+      <span>L</span>
+      <span>i</span>
+      <span>g</span>
+      <span>h</span>
+      <span>t</span>
+    </div>
+  );
+}
+
+const meta: Meta<typeof NeonLightText> = {
+  component: NeonLightText,
+  parameters: { darkMode: { current: "dark" } },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof NeonLightText>;
+
+export const Normal: Story = {
+  args: {
+    // className:
+    className: styles.container,
+  },
+};
+
+// TODO: add type variants
+
+// TODO: add color variants
